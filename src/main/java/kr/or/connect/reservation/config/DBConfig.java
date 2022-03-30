07260dd9a0b08138f7f4d57bfc3cc9d172c0c785
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2022-03-30 21:44:16 +0900

    Add application.properties

--- src/main/java/kr/or/connect/reservation/config/DBConfig.java
+++ src/main/java/kr/or/connect/reservation/config/DBConfig.java
@@ -3,22 +3,28 @@ package kr.or.connect.reservation.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+@PropertySource("classpath:application.properties")
 @Configuration
 @EnableTransactionManagement
 public class DBConfig implements TransactionManagementConfigurer {
-	private String driverClassName = "com.mysql.jdbc.Driver";
-
-	private String url = "jdbc:mysql://localhost:3306/reservationdb?useUnicode=true&characterEncoding=utf8";
-
-	private String username = "connectuser";
-	private String password = "connect123!@#";
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;// = "com.mysql.jdbc.Driver";
+	@Value("${spring.datasource.url}")
+	private String url;// = "jdbc:mysql://localhost:3306/reservationdb?useUnicode=true&characterEncoding=utf8";
+	@Value("${spring.datasource.username}")
+	private String username;// = "connectuser";
+	@Value("${spring.datasource.password}")
+	private String password;// = "connect123!@#";
 
 	@Bean
 	public DataSource dataSource() {
